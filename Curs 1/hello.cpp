98a7d3ce7555eb865ba8bec4263c4e94f52510f4
Robert <cartoful.99@gmail.com>
2023-02-12 18:21:13 +0200

    Am adaugat comenatarii!

--- Curs 1/hello.cpp
+++ Curs 1/hello.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 int main()
-{
+{   
+    // Declara o variabila de tip intreg cu numele de "varsta"
     int varsta;
+    // Ii afiseaza user ului ce sa introduca
     cout << "Te rog introdu o varsta : ";
+    // Citeste de la tastatura varsta user ului
     cin >> varsta;
 
+    // Daca userul are peste 18 ani face prima chestie
     if(varsta >= 18){
         cout << "Da, tipa are " << varsta << " ani!";
     }
+    //Altfel face chestia asta
     else{
         cout << "Nu, este mai mica de 18 ani!";
     }
--- Curs 1/whileLoop2.cpp
+++ Curs 1/whileLoop2.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 int main()
 {
-    // Declar variabila de tip caracter cu numele de "caracter"    
+    // Declar variabila de tip caracter cu numele de "caracter"
     char caracter;
-    //
+    // Afiseaza ce sa introduca user ul
     cout << "Intodu un caracter : ";
+    // Citeste de la tastura input ul user ului
     cin >> caracter;
 
+    // Verifica cat timp valoarea variabile "caracter" este diferita de caracterul "s"
     while (caracter != 's')
     {
+        // Afiseaza ca user ul a gresit litera
         cout << "Ai gresit litera!" << endl;
-        
+
+        // Ii cere user ului o litera noua sa introduca
         cout << "Intodu un caracter : ";
         cin >> caracter;
     }
+    // Anunta user ul ca a nimerit litera!
     cout << "Ai nimerit litera!";
 }
